4beef3c34669fd53ac18991c4b01ede38b9aa3c7
santi-dev1 <santirojass2004@gmail.com>
2024-09-09 20:24:27 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+document.getElementById('learn-more').addEventListener('click', function() {
+    const aboutSection = document.getElementById('about');
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+});
